033bdae5739fabf31d23b3f702c3009917ca2597
JoseBambora <zebragapt@gmail.com>
2022-03-29 19:08:40 +0100

    Learned to use dups and updated the code in order to use it, since it is necessary. Also updated some things that were incompleted.

--- bcompress.c
+++ bcompress.c
@@ -5,7 +5,9 @@
 
 
 int main(int argc, char** argv){
-	execlp("bzip2","bzip2","-k",argv[1],NULL);
+    int fileWrite = open(argv[2],O_WRONLY);
+	dup2(fileWrite,1);
+	execlp("bzip2","bzip2","-c","-k",argv[1],NULL);
 	perror("error executing command");
 	return 0;
 }
--- sdstore.c
+++ sdstore.c
@@ -1,19 +1,43 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <string.h>
 
+void execname(char *src, char *result)
+{
+    int i,j;
+    result[0] = '.';
+    result[1] = '/';
+    for(i = 0, j = 2; src[i] != '\0'; i++, j++)
+        result[j] = src[i];
+    result[j] = '\0';
+}
+
+void applyexec(char *exec_src, char*f1, char*f2)
+{
+    int i;
+    for(i = 0; exec_src[i] != '\0'; i++);
+    char exec[i+3];
+    execname(exec_src,exec);
+    execlp(exec,exec,f1,f2,NULL);
+}
 
 int main(int argc, char** argv)
 {
     write(STDIN_FILENO,"Processing\n",11);
     int fileWrite = open(argv[2],O_CREAT | O_TRUNC | O_WRONLY, 0660);
     close(fileWrite);
-    for(int i = 3; i < argc; i++)
+    if(argc > 3)
     {
         if(fork() == 0)
-            execlp(argv[i],argv[i],argv[1],argv[2],NULL);
+            applyexec(argv[3],argv[1],argv[2]);
+    }
+    for(int i = 4; i < argc; i++)
+    {
+        if(fork() == 0)
+            applyexec(argv[i],argv[2],argv[2]);
     }
     for(int i = 3; i < argc; i++)
         wait(NULL);
-    write(STDIN_FILENO,"Conclued\n",9);
+    write(STDIN_FILENO,"concluded\n",10);
 }
