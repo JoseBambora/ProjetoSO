3f9eb2ac2174a6db1499d6591dbb8749d0232482
JoseBambora <zebragapt@gmail.com>
2022-03-29 19:17:53 +0100

    Forgot to add this before.

--- bdecompress.c
+++ bdecompress.c
@@ -3,7 +3,9 @@
 #include <fcntl.h>
 
 int main(int argc, char** argv){
-	execlp("bzip2","bzip2","-k","-d",argv[1],NULL);
+    int fileWrite = open(argv[2],O_WRONLY);
+	dup2(fileWrite,1);
+	execlp("bzip2","bzip2","-c","-k","-d",argv[1],NULL);
 	perror("error executing command");
 	return 0;
 }
